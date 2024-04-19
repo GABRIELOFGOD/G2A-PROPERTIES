2f46521306f24e719f11bbe11f11d22f48472865
GABRIELOFGOD <gabrielayodele2021@gmail.com>
2024-04-20 00:36:23 +0100

    CORS AND COOKIE

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const app = express()
 
 const PORT = process.env.PORT || 4200
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174', 'https://g2-a-properties.vercel.app', '*'],
+    origin: 'http://localhost:5173',
     credentials: true
 }))
 
